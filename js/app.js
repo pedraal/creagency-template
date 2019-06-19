57b46325baacf82c3e7e15b39b1f87b4f2e443ea
Pierre GOLFIER <pgolfier.pro@gmail.com>
2019-06-19 13:04:40 +0200

    v1.0

---
+++ js/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    let body = $(document)
+    let nav = $("nav")
+    let navburger = $(".navbar-burger")
+    let navLinks = $(".navbar a, footer a")
+
+    if (body.scrollTop() > 10) {
+        nav.addClass("nav-scrolled");
+    } else {
+        nav.removeClass("nav-scrolled");
+    }
+    body.on("scroll", function() {
+        if (body.scrollTop() > 10) {
+            nav.addClass("nav-scrolled");
+        } else {
+            nav.removeClass("nav-scrolled");
+        }
+    })
+
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '40px',
+        arrows: true,
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        focusOnSelect: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    centerPadding: '60px',
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    centerPadding: '20px',
+                }
+            }
+        ]
+    });
+
+    navburger.on("click", function() {
+        $('.navbar-menu').toggleClass("is-active")
+    })
+
+    navLinks.on("click", function(e) {
+        e.preventDefault()
+        let hash = this.hash
+        $("body,html").animate({ scrollTop: $(hash).offset().top }, 900, () => window.location.hash = hash)
+    })
+
+})
